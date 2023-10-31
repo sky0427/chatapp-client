95f3fc3dbd0d7ef7111b5ce4c1a8ede1f8b224eb
sukeun95 <sukeun95@gmail.com>
2023-10-31 19:40:03 +0900

    chatapp-client v1

--- src/App.js
+++ src/App.js
@@ -1,32 +1,49 @@
 import { useEffect, useState } from 'react';
-import "./App.css";
+import './App.css';
 import socket from './server';
+import InputField from './components/InputField/InputField';
+import MessageContainer from './components/MessageContainer/MessageContainer';
 
 function App() {
-  const [user, setUser] = useState("");
+	const [user, setUser] = useState('');
+	const [message, setMessage] = useState('');
+	const [messageList, setMessageList] = useState([]);
 
-  useEffect(() => {
-    askUserName();
-  },[]);
+	useEffect(() => {
+		socket.on('message', (message) => {
+			setMessageList((prevState) => prevState.concat(message));
+		});
+		askUserName();
+	}, []);
 
-  const askUserName = () => {
-    const userName = prompt("당신의 이름을 입력하세요.");
-    console.log("유저이름 :", userName);
+	const askUserName = () => {
+		const userName = prompt('당신의 이름을 입력하세요.');
+		console.log('유저이름 :', userName);
 
-    socket.emit("login", userName, (res) => {
-      if (res?.ok) {
-        setUser(res.data);
-        console.log(user);
-      }
-      console.log("Res",res);
-    });
-  };
+		socket.emit('login', userName, (res) => {
+			if (res?.ok) {
+				setUser(res.data);
+				console.log(user);
+			}
+			console.log('Res', res);
+		});
+	};
 
-  return (
-    <div>
-      <div className="App"></div>
-    </div>
-  );
+	const sendMessage = (event) => {
+		event.preventDefault();
+		socket.emit('sendMessage', message, (res) => {
+			console.log('sendMessage res', res);
+		});
+	};
+
+	return (
+		<div>
+			<div className="App">
+				<MessageContainer messageList={messageList} user={user} />
+				<InputField message={message} setMessage={setMessage} sendMessage={sendMessage} />
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/InputField/InputField.jsx
+++ src/components/InputField/InputField.jsx
@@ -1,31 +1,26 @@
-import React from 'react'
-import { Input } from "@mui/base/Input";
-import { Button } from "@mui/base/Button";
-import './InputField.css'
-const InputField = ({message,setMessage,sendMessage}) => {
+import React from 'react';
+import { Input } from '@mui/base/Input';
+import { Button } from '@mui/base/Button';
+import './InputField.css';
+const InputField = ({ message, setMessage, sendMessage }) => {
+	return (
+		<div className="input-area">
+			<div className="plus-button">+</div>
+			<form onSubmit={sendMessage} className="input-container">
+				<Input
+					placeholder="Type in here…"
+					value={message}
+					onChange={(event) => setMessage(event.target.value)}
+					multiline={false}
+					rows={1}
+				/>
 
-  return (
-    <div className="input-area">
-          <div className="plus-button">+</div>
-          <form onSubmit={sendMessage} className="input-container">
-            <Input
-              placeholder="Type in here…"
-              value={message}
-              onChange={(event) => setMessage(event.target.value)}
-              multiline={false}
-              rows={1}
-            />
+				<Button disabled={message === ''} type="submit" className="send-button">
+					전송
+				</Button>
+			</form>
+		</div>
+	);
+};
 
-            <Button
-              disabled={message === ""}
-              type="submit"
-              className="send-button"
-            >
-              전송
-            </Button>
-          </form>
-        </div>
-  )
-}
-
-export default InputField
+export default InputField;
